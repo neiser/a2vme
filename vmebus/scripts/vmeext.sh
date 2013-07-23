aae76fa170382e3e161b7bebaf08368019cb3366
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-07-23 14:50:21 +0200

    Fixing the script, no absolute paths!

--- vmebus/scripts/vmeext.sh
+++ vmebus/scripts/vmeext.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 #echo `/home/acqu/VUPROM/vmebus/vmeext $1 $((0x$2)) $3 | cut -f 6 -d " " `
 set mytest=`printf '%x' $2`
 echo -n "0x"
-echo `/home/acqu/VUPROM/vmebus/vmeext $1 $mytest $3 | cut -f 6 -d " " `
-
+echo `vmeext $1 $mytest $3 | cut -f 6 -d " " `
