c1eaea8b3e41ada1956c2f1edabb7706cf2d7e48
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-03-20 15:47:51 +0100

    Reading and writing seems to work (reverse engineered from old code)

--- sadc_test/src/sadc_test.cpp
+++ sadc_test/src/sadc_test.cpp
@@ -258,13 +258,47 @@ bool i2c_write(vme32_t gesica, UInt_t bytes, UInt_t addr, UInt_t data) {
   return i2c_wait(gesica, true);
 }
 
-bool i2c_read_reg(vme32_t gesica, UInt_t adc_side, UInt_t reg, UInt_t& data) {
-  UInt_t addr = 0x40 + (adc_side << 5) + reg;
+UInt_t i2c_make_reg_addr(UInt_t adc_side, UInt_t reg) {
+  // in order to access the registers of the
+  // ZR chips via the HL chip on the SADC,
+  // we set the highest bit6 in i2c addr
+  UInt_t addr = 1 << 6;
+  // select the chip via bit5 (adc_side=1 and adc_side=0)
+  addr |= (adc_side & 0x1) << 5;
+  // and set the actual register (bit4-bit0)
+  addr |= reg & 0x1f;
+  return addr;
+}
 
-  if(!i2c_write(gesica, 2, addr, 0))
+bool i2c_read_reg(vme32_t gesica, UInt_t adc_side, UInt_t reg, UInt_t& data) {
+
+  UInt_t addr = i2c_make_reg_addr(adc_side, reg);
+
+  // this write before reading is really necessary...
+  // does it tell the HL chip to access this address of one of the ZRs?
+  if(!i2c_write(gesica, 1, addr, 0))
     return false;
 
+  // read the response, maximum two bytes
   if(!i2c_read(gesica, 2, addr, data))
+    return false;
+
+  return true;
+}
+
+bool i2c_write_reg(vme32_t gesica, UInt_t adc_side, UInt_t reg, UInt_t data) {
+
+  UInt_t addr = i2c_make_reg_addr(adc_side, reg);
+
+  // tell the HL chip to access this address of the ZR?
+  // this was not in the old code, but a complete i2c_read_reg read was before that
+  // to make it more reliable........?!?!?!
+  //if(!i2c_write(gesica, 1, addr, 0))
+  //  return false;
+
+  // construct our data, the lower 8 bits must be zero?
+  UInt_t i2c_data = data << 8;
+  if(!i2c_write(gesica, 3, addr, i2c_data))
     return false;
 
   return true;
@@ -353,13 +387,17 @@ int main(int argc, char *argv[])
     *(gesica+0x20/4) |= 1 << (port_id+16);
   }
 
+  // set some registers
+  i2c_set_port(gesica, 5, false);
+  i2c_write_reg(gesica, 0, 0x10, 0xf);
+
   // read some registers
   for(UInt_t port_id=0;port_id<6;port_id++) {
-    // no broadcast mode => access single SADCs
+    // no broadcast mode => access single SADCs?
     i2c_set_port(gesica, port_id, false);
 
 
-    for(UInt_t reg=0x0;reg<0x3;reg++) {
+    for(UInt_t reg=0x10;reg<0x11;reg++) {
       UInt_t data;
       i2c_read_reg(gesica, 0, reg, data);
 
