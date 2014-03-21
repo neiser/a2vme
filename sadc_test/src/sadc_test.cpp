db589560d6dc2a80356d2c9a75ec5e67673381f0
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-03-21 15:50:26 +0100

    Programming of Gesica added, but TCS clock not locked afterwards...

--- sadc_test/src/sadc_test.cpp
+++ sadc_test/src/sadc_test.cpp
@@ -354,7 +354,7 @@ bool i2c_program_sadc(vme32_t gesica, const char* rbt_filename) {
   if(!i2c_write(gesica, 1, 2, 0x0))
     return false;
 
-  // set program bit
+  // set program bit = bit2
   if(!i2c_write(gesica, 1, 2, 0x4))
     return false;
 
@@ -366,14 +366,15 @@ bool i2c_program_sadc(vme32_t gesica, const char* rbt_filename) {
       return false;
     nTries++;
     if(nTries==10000) {
-      cerr << "Reached maximum wait time for init programming. Last status = " << status << endl;
+      cerr << "Reached maximum wait time for init programming. "
+           << "Last status = " << hex << status << dec << endl;
       return false;
     }
   }
   while((status & 0x1) == 0);
 
-  cout << "Programming SADC..." << endl;
   // write the file, 2 bytes at once
+  cout << "Programming SADC..." << endl;
   for(UInt_t i=0;i<rbt_data.size();i+=2) {
     UInt_t i2c_data = (rbt_data[i+1] << 8) + rbt_data[i];
     if(!i2c_write(gesica, 2, 3, i2c_data)) {
@@ -384,13 +385,92 @@ bool i2c_program_sadc(vme32_t gesica, const char* rbt_filename) {
       cout << "." << flush;
   }
   cout << endl;
+
+
+  // check status, bit1 = FPGA0 done, bit3 = FPGA1 done
+  if(!i2c_read(gesica, 1, 2, status))
+    return false;
+  if(status != 0xe) {
+    cerr << "Status = " << hex << status  << dec
+         << " != 0xe (not both FPGAs indicate DONE), programming failed." << endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool i2c_program_gesica(vme32_t gesica, const char* rbt_filename) {
+  vector<UInt_t> rbt_data;
+  if(!load_rbt(rbt_filename, rbt_data))
+    return false;
+
+  // the CPLD register 0x14 is used to program the GeSiCa FPGA
+  // there are three relevant bits (see manual)
+
+  // init chip
+  *(gesica+0x14/4) = 0x7;
+  *(gesica+0x14/4) = 0x2;
+
+  // wait for init high (bit0)
+  UInt_t nTries = 0;
+  while((*(gesica+0x14/4) & 0x1) == 0) {
+    nTries++;
+    if(nTries==1000) {
+      cerr << "Reached maximum wait time for init programming. "
+           << "Last value 0x14 = " << hex << *(gesica+0x14/4) << dec << endl;
+      return false;
+    }
+  }
+  UInt_t status = *(gesica+0x14/4);
+  cout << "Start programming, status = 0x" << hex << status << dec << endl;
+
+
+  // transmit data bitwise, per byte msb first
+  for(UInt_t i=0;i<rbt_data.size();i++) {
+    for(int j=7;j>=0;j--) { // j must be signed here
+      UInt_t bit = (rbt_data[i] >> j) & 0x1;
+      // first write the bit, set CCLK low
+      UInt_t datum = (bit << 2);
+      *(gesica+0x14/4) = datum;
+      // then set CCLK => rising edge of CCLK
+      datum |= 0x2;
+      *(gesica+0x14/4) = datum;
+    }
+    if(i % (1 << 14) == 0)
+      cout << "." << flush;
+  }
+  cout << endl;
+
+  // check status again
+  status = *(gesica+0x14/4);
+  if((status & 0x2) == 0) {
+    cerr << "Status bit1 not high (not DONE), programming failed." << endl;
+    return false;
+  }
+  cout << "Programming done, status = 0x" << hex << status << dec << endl;
+
+  // try resetting the module..?!
+  *(gesica+0x0/4) = 1;
+
+  // wait for TCS clock to be locked
+  nTries = 0;
+  while((*(gesica+0x20/4) & 0x1) == 0) {
+    nTries++;
+    if(nTries==10000000) {
+      cerr << "Reached maximum wait time for TCS clock lock. "
+           << "Last value 0x20 = " << hex << *(gesica+0x20/4) << dec << endl;
+      return false;
+    }
+  }
+
+
   return true;
 }
 
 int main(int argc, char *argv[])
 {
-  if (argc > 2) {
-    cerr << "This program does not take more than one argument." << endl;
+  if (argc > 3) {
+    cerr << "This program does not take more than two arguments." << endl;
     exit(EXIT_FAILURE);
   }
   
@@ -416,6 +496,11 @@ int main(int argc, char *argv[])
     cerr << "Error opening VME access to GeSiCa." << endl;
     exit (EXIT_FAILURE);
   }
+
+  if(argc==3) {
+    i2c_program_gesica(gesica, argv[2]);
+  }
+
   // the module ID is at 0x0
   cout << "# GeSiCa Firmware (should be 0x440d5918): 0x"
        << hex << *(gesica+0x0/4) << dec << endl;
@@ -432,11 +517,13 @@ int main(int argc, char *argv[])
   UInt_t gesica_SCR = *(gesica+0x20/4);
   // check if clocks are locked (if not there's a TCS problem)
   if((gesica_SCR & 0x1) == 0) {
-    cerr << "TCS clock not locked" << endl;
+    cerr << "TCS clock not locked: Status = 0x"
+         << hex << gesica_SCR << dec << endl;
     exit(EXIT_FAILURE);
   }
   if((gesica_SCR & 0x2) == 0) {
-    cerr << "Internal clocks not locked" << endl;
+    cerr << "Internal clocks not locked. Status = 0x"
+         << hex << gesica_SCR << dec << endl;
     exit(EXIT_FAILURE);
   }
 
